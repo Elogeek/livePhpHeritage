f1ea6fb31b78d64db92e35512cc32fc24cb8151e
elogeek <elodiechristin@gmail.com>
2021-04-12 09:41:50 +0200

    gogogo

---
+++ classes/Android.php
@@ -0,0 +1,10 @@
+<?php
+
+class android extends Smartphone
+{
+public function __construct(string $brd, string $operateur)
+{
+parent::__construct($brd, 'Android', $operateur);
+
+}
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require './classes/User.php';
+require './classes/Computer.php';
+require './classes/Smartphone.php';
+require './classes/Android.php';
+
+$user = new User('John Doe', 'password très fort');
+
+$computerOne = new Computer('Lenovo', 'Linux');
+$computerOne->setOwner($user);
+
+$smartphone = new Smartphone('Huawai', 'Android', 'free');
+$smartphone->start();
+
+$android = new Android('Huawai', 'Free');
